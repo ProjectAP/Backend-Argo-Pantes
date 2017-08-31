aaca465f21de3f5a4b6645b2d6bf92b06cad64b8
wedaAP <weda.dewa@argopantes.com>
2017-08-31 13:47:30 +0700

    create table users

--- database/migrations/2017_08_24_032548_create_table_users.php
+++ database/migrations/2017_08_24_032548_create_table_users.php
@@ -13,8 +13,37 @@ class CreateTableUsers extends Migration
      */
     public function up()
     {
-        Schema::table('Users', function (Blueprint $table) {
-            //
+        Schema::create('Employees', function (Blueprint $table) {
+            $table->string('nik')->primary('nik')->index();
+            $table->string('name');
+            $table->integer('comp');
+            // $table->foreign('comp')->references('code')->on('Company');
+            $table->integer('dept');
+            // $table->foreign('dept')->references('code')->on('Department');
+            /*$table->integer('section');
+            $table->foreign('section')->references('code')->on('Section');*/
+            $table->integer('pos');
+            // $table->foreign('pos')->references('code')->on('Position');
+            $table->string('ext');
+            $table->string('phone');
+            $table->integer('gender');
+            $table->string('photo');
+            $table->string('mother');
+        });
+
+        Schema::create('Level_user', function (Blueprint $table) {
+            $table->integer('code')->primary('code')->index();
+            $table->string('name');
+            $table->text('desc');
+
+        });
+
+        Schema::create('Users', function (Blueprint $table) {
+            $table->string('email')->primary('email')->index();
+            $table->string('nik')->foreign('nik')->references('nik')->on('Employees');
+            $table->string('pass');
+            $table->integer('level')->foreign('level')->references('code')->on('Level_user');
+            $table->integer('status')->unsigned()->default(0);
         });
     }
 
@@ -25,8 +54,8 @@ class CreateTableUsers extends Migration
      */
     public function down()
     {
-        Schema::table('Users', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('Users');
+        Schema::dropIfExists('Level_user');
+        Schema::dropIfExists('Employees');
     }
 }
